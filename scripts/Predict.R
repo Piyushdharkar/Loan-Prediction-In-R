b4d7e13cbc65ef3205f60e60a2fce470818efe29
Piyush <Piyush.Dharkar@gmail.com>
2018-08-30 15:12:37 +0530

    Removed dummification step, changed classifier to Adaboost.

--- scripts/Predict.R
+++ scripts/Predict.R
@@ -7,35 +7,15 @@ predict_data = function(input_dataset, pp) {
   colnames(dataset) <- feature_list
   
   dataset <- input_dataset[feature_list]
-  
-  #Dummify
-  cont_var <- c('ApplicantIncome', 'CoapplicantIncome', 'LoanAmount')
-  cat_var <- dataset %>% select(-cont_var) %>% colnames()
-  
-  categorical <- dataset[cat_var]
-  continuous <- dataset %>% select(cont_var)
-  
-  dmy <- dummyVars( ~ ., data = categorical)
-  
-  new_categorical <- data.frame(predict(dmy, newdata = categorical)) %>% 
-    select(-Gender., -Married., -Dependents., -Self_Employed.)
-  
-  
-  dummified_dataset <- cbind(continuous, new_categorical)
-  
-  #################3
-  #print(dummified_dataset)
-  
+
   
   #Preprocess
-  categorical <- dummified_dataset %>% select(-cont_var)
-  continuous <- dummified_dataset %>% select(cont_var)
+  categorical <- dataset %>% select(-cont_var)
+  continuous <- dataset %>% select(cont_var)
   
-  new_dataset <-predict(pp, continuous) %>% cbind(categorical)
+  new_dataset <- predict(pp, continuous) %>% cbind(categorical)
   
-  #################
-  #print(new_dataset)
   
   #Load model and predict
-  readRDS("model.rds") %>% predict(dummified_dataset)
+  readRDS("model.rds") %>% predict(new_dataset)
 }
